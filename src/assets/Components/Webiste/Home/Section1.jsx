70de47d97f51c49d0736d4066620af17f242959d
salman <therodfighter@gmail.com>
2023-04-19 20:26:43 +0400

    vsfs

--- src/assets/Components/Webiste/Home/Section1.jsx
+++ src/assets/Components/Webiste/Home/Section1.jsx
@@ -5,9 +5,9 @@ const Section1 = () => {
   const navigate = useNavigate();
 
   return (
-    <section className=" flex flex-col lg:flex-row lg:h-[820px] w-full  bg-[#EAEFF3]  ">
+    <section className=" flex flex-col lg:flex-row  lg:h-[820px] w-full  bg-[#EAEFF3]  ">
       {/* left side */}
-      <div className="flex  w-full text-start  items-start justify-center py-10  flex-col gap-2 respnsive-pading-1">
+      <div className="flex  w-full grow text-start  items-start justify-center py-10  flex-col gap-2 respnsive-pading-1">
         <Typography variant="h72" className="leading-[80px]">
           Your professional dry clean & laundry service
         </Typography>
@@ -41,20 +41,23 @@ const Section1 = () => {
         </div>
       </div>
       {/* right side */}
-      <div className="min-h-[450px] relative  xl:max-w-[860px] xl:min-w-[860px] bg-[#02A5BF]">
-        <div className="absolute flex flex-col p-7   h-auto  gap-6 xl:left-[-15%] bottom-[10%] max-w-[700px] 2xl:max-w-[800px] z-20  border-[#03444F] rounded-2xl ">
+      <div className="min-h-[400px] sm:min-h-[450px] relative  min-w-[50%] xl:min-w-[860px] bg-[#02A5BF]">
+        <div className="absolute flex flex-col p-7   h-auto  gap-6 left-[5%] sm:left-[10%] lg:[18%]   xl:left-[-15%] bottom-[19%] sm:bottom-[18%] xl:bottom-[10%] max-w-[700px] 2xl:max-w-[800px] z-20  border-[#03444F] rounded-2xl ">
           <img
             src={import.meta.env.VITE_BASE_URL + "/images/section1-art2.png"}
-            className=""
+            className="h-50 sm:h-80 lg:h-auto"
             alt=""
+            onClick={() => {
+              navigate("/dashboard/ordernow/address");
+            }}
           />
         </div>
         {/* leaves svg */}
-        <div className="absolute right-0 bottom-[-15px]">
+        <div className="absolute right-0 bottom-[-15px] z-10">
           <img
             src={import.meta.env.VITE_BASE_URL + "/images/leaves.svg"}
             alt=""
-            className=""
+            className=" h-56 md:h-auto"
           />
         </div>
 
@@ -63,10 +66,11 @@ const Section1 = () => {
           <img
             src={import.meta.env.VITE_BASE_URL + "/images/stars.svg"}
             alt=""
+            className="h-56 lg:h-auto"
           />
         </div>
         {/* reviews */}
-        <div className="hidden absolute md:flex text-white gap-2 flex-wrap font-[700] bottom-10 left-16">
+        <div className="absolute md:flex text-white gap-2 flex-wrap font-[700] bottom-6 sm:bottom-10 left-6 sm:left-16">
           <div className="flex">
             <img
               src={import.meta.env.VITE_BASE_URL + "/images/star.svg"}
